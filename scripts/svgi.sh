6a4337f376286b7404a7c84a74209d3441cce6f2
xanedarel <94392748+xanedarel@users.noreply.github.com>
2024-07-11 04:24:19 +0200

    Update svgi.sh

--- scripts/svgi.sh
+++ scripts/svgi.sh
@@ -2,7 +2,6 @@
 
 #this is the grub file that will be edited
 grubd=40_custom
-#/>
 
 #update the preexisting mementry in 40_custom
 cp /etc/grub.d/$grubd /home/$user/.cache/$grubd
@@ -11,9 +10,11 @@ echo "Latest kernel found : $varcurrentlinux"
 sed -i "s/[0-9]\.[0-9]\.[0-9]*_[0-9]/$varcurrentlinux/g" /etc/grub.d/40_custom
 update-grub
 
+# >
 #update the no-vfio.img using dracut
 mv /etc/dracut.conf.d/20-vfio.conf /etc/
 dracut --force /boot/no-vfio.img
 #move back 20-vfio.conf into /etc/dracut.conf.d/
 mv /etc/20-vfio.conf /etc/dracut.conf.d/
-
+#this is ugly, there must be a better way
+#/>
